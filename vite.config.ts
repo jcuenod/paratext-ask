84907f1121a71ce7e9072cc27910e3f96ed8c000
jcuenod <4253884+jcuenod@users.noreply.github.com>
2024-05-07 09:34:04 -0500

    More deploy attempts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import path from "path"
+import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true
-    }
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    }
-  }
+	plugins: [react()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	base: "/paratext-ask/",
 })
